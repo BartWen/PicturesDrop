9f53f6fe288ddd07c85fc44b3239540b0aa49fc8
BartWen <124913970+BartWen@users.noreply.github.com>
2023-05-22 19:45:35 +0200

    Add project files.

---
+++ PicturesDrop/Main.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PicturesDrop
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnAddPhoto = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(739, 622);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            
+            // 
+            // btnAddPhoto
+            // 
+            this.btnAddPhoto.Location = new System.Drawing.Point(12, 12);
+            this.btnAddPhoto.Name = "btnAddPhoto";
+            this.btnAddPhoto.Size = new System.Drawing.Size(87, 23);
+            this.btnAddPhoto.TabIndex = 1;
+            this.btnAddPhoto.Text = "Dodaj zdjęcie";
+            this.btnAddPhoto.UseVisualStyleBackColor = true;
+            this.btnAddPhoto.Click += new System.EventHandler(this.btnAddPhoto_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(106, 13);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(770, 675);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddPhoto);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Main";
+            this.Text = "PictureDrop";         
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnAddPhoto;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ PicturesDrop/Main.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PicturesDrop
+{
+    public partial class Main : Form
+    {
+
+        private OpenFileDialog openFile;
+        private string _sourceFile;
+
+        public Main()
+        {
+            InitializeComponent();
+            GetFilePath();
+            PathFile();
+            DeleteBtnVisible();
+        }
+        private void DeleteBtnVisible()
+        {
+            if (pictureBox.Image != null)
+
+                btnDelete.Visible = true;
+            else
+                btnDelete.Visible = false;
+
+        }
+        private void GetFilePath()
+
+        {
+            if (File.Exists(PathFile()))
+            {
+                _sourceFile = File.ReadAllText(PathFile());
+                if (_sourceFile == string.Empty)
+                {
+                    return;
+                }
+                this.pictureBox.Image = Image.FromFile(_sourceFile);
+            }
+        }
+        private string PathFile()
+        {
+            var pathFile = $@"{Path.GetDirectoryName(Application.ExecutablePath)}\UserPath.txt";
+
+            if (!File.Exists(pathFile))
+                File.Create(pathFile);
+
+            return pathFile;
+
+        }
+        private void btnAddPhoto_Click_1(object sender, EventArgs e)
+        {
+            openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                this.pictureBox.Image = Image.FromFile(openFile.FileName);
+            }
+
+            var sourceFile = openFile.ToString();
+
+            _sourceFile = sourceFile.Remove(0, 56);
+
+            File.WriteAllText(PathFile(), _sourceFile);
+
+            DeleteBtnVisible();
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(PathFile(), string.Empty);
+
+            this.pictureBox.Image = null;
+
+            DeleteBtnVisible();
+        }
+    }
+
+}
